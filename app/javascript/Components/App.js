99b063434b716304fce53c1385e4c9e8de28d88e
bruk19 <brukteshome21@gmail.com>
2022-11-02 08:37:35 +0300

    Update app component

--- app/javascript/Components/App.js
+++ app/javascript/Components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "../Redux/store";
+import Greeting from "./Greeting";
 
-function App() {
-  return <h1>Hello Rails-React</h1>;
-}
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Greeting />
+    </Provider>
+  );
+};
 
 export default App;
