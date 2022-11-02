b3f5daaab0c239762506388c68854f63535a62c3
bruk19 <brukteshome21@gmail.com>
2022-11-02 08:36:40 +0300

    Add component of greeting

---
+++ app/javascript/Components/Greeting.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { getMessage } from "../Redux/Reducer";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const message = useSelector((state) => state.greeting);
+
+  async function fetchMessage() {
+    await axios.get("api/messages").then((response) => {
+      dispatch(getMessage(response.data.greeting));
+    });
+  }
+
+  useEffect(() => {
+    fetchMessage();
+  }, []);
+
+  return <h1>{message}</h1>;
+};
+
+export default Greeting;
